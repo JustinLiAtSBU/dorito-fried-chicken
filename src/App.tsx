e74e8dff0466fcc173dcf00a6cbb4652b217ad16
Justin <justinli.9876@gmail.com>
2021-12-10 21:20:35 -0500

    Change favicon to drumstick

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
-import './index.css'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+      <h1 className="text-3xl font-bold">
+        Dorito Fried Chicken
       </h1>
     </div>
   );
